87b8cbb46d757781d7e3a5a7cf8c7f37b6edf547
Hanul <hanul@hanul.me>
2015-06-16 23:20:24 +0900

    no message

--- include.php
+++ include.php
@@ -3,8 +3,13 @@
 // 플라즈마가 설치된 폴더입니다.
 $PLASMA_FOLDER_PATH = dirname(__FILE__);
 
+
 // 유틸리티 로드
+include $PLASMA_FOLDER_PATH.'/util/RequestInfo.php';
+include $PLASMA_FOLDER_PATH.'/util/Validator.php';
+
 include $PLASMA_FOLDER_PATH.'/util/redirect.php';
+
 
 // MySQL 관련 기능 로드
 include $PLASMA_FOLDER_PATH.'/mysql/mysql_get_one.php';
