d96892994ec6017b580645fca8e5d2d70d092d76
TaeMin <wwwee98@gmail.com>
2015-06-26 03:51:01 +0900

    completing modelbox, modelobject

--- model/ModelBox.php
+++ model/ModelBox.php
@@ -2,7 +2,6 @@
 
 class Plasma_ModelBox extends Plasma_BaseModel
 {
-    //ToyBox
 
     function __construct()
     {
@@ -11,96 +10,9 @@ class Plasma_ModelBox extends Plasma_BaseModel
         $this->id = 0;
     }
 
-
-    public function save()
+    public function createEmptyObject()
     {
-        // execute query to save
-
-        // generate all column's sql query
-        $columnNames = '';
-        $columnValues = '';
-        foreach ($this->columns as $fieldName => $columnObj)
-        {
-            if ($columnObj->columnName == null) {
-                $columnNames .= $fieldName.', ';
-            } else {
-                $columnNames .= $columnObj->columnName.', ';
-            }
-            $columnValues .= $columnObj->generateValueForSQL().', ';
-        }
-        $columnNames = substr($columnNames, 0, -1);
-        $columnValues = substr($columnValues, 0, -1);
-        $thisTableName = $this->tableName;
-        $thisId = $this->id;
-
-        if ($this->id == 0) {
-            // This object is not saved yet.
-            $query = 'INSERT INTO $thisTableName ($columnNames) VALUES ($columnValues);';
-            // Execute query
-        } else {
-            // This object is on the DB.
-            $query = 'INSERT INTO $thisTableName ($columnNames) VALUES ($columnValues) WHERE id=$thisId;';
-            // Execute query
-        }
-
-        mysql_run_query($query);
-
-    }
-
-    public function update($fieldName, $_value)
-    {
-        if ($this->id == 0)
-        {
-            throw new Plasma_ObjectNotExists;
-        }
-
-        try
-        {
-            /*
-             * variable columns forms this structure -
-             * fieldName => columnObject(contains type, value, columnName)
-             */
-            if (array_key_exists($fieldName, $this->columns))
-            {
-                // make query to update
-                $this->columns[$fieldName]->setValue($_value);
-
-                $thisTableName = $this->tableName;
-                $columnName = $this->columns[$fieldName]->columnName;
-                if ($columnName == null) {
-                    $columnName = $fieldName;
-                }
-                $columnValue = $this->columns[$fieldName]->generateValueForSQL();
-                $thisId = $this->id;
-                $query = 'UPDATE $thisTableName SET $columnName=$columnValue WHERE id=$thisId';
-
-                // Execute Query
-                mysql_run_query($query);
-
-            }
-            else
-            {
-                throw new Plasma_noColumnException;
-            }
-
-        }
-        catch(Plasma_noColumnException $e)
-        {
-            // No column.
-        }
-    }
-
-    public function delete()
-    {
-        if ($this->id == 0)
-        {
-            throw new Plasma_ObjectNotExists;
-        }
-        $thisTableName = $this->tableName;
-        $thisId = $this->id;
-        $query = 'DELETE FROM $thisTableName WHERE id=$thisId';
-        // Execute Query
-        mysql_run_query($query);
+        return new Plasma_ModelObject($this, $this->columns);
     }
 
     public function find($findArray)
@@ -131,6 +43,20 @@ class Plasma_ModelBox extends Plasma_BaseModel
         $query = 'SELECT * FROM $thisTableName WHERE $findQuery;';
 
         $list = mysql_get_list($query);
+        $returnArray = array();
+        /*
+         * columnName => value
+         */
+        foreach ($list as $singleRow)
+        {
+            $_columns = $this->columns;
+            foreach ($singleRow as $columnName => $value)
+            {
+                $_columns[$columnName]->setValue($value);
+            }
+            array_push($returnArray, new Plasma_ModelObject($this, $_columns));
+        }
+        return $returnArray;
         // TODO: 이 값들을 해당 모델에다가 넣어주고, 객체로 리턴해야 함.
     }
 
--- model/ModelObject.php
+++ model/ModelObject.php
@@ -1,2 +1,103 @@
 <?php
- 
+class Plasma_ModelObject extends Plasma_BaseModel
+{
+
+    function __construct($_modelBox, $_columns)
+    {
+        $this->modelBox = $_modelBox;
+        $this->tableName = $_modelBox->tableName;
+        $this->columns = $_columns;
+        $this->id = 0;
+    }
+
+    public function save()
+    {
+        // execute query to save
+
+        // generate all column's sql query
+        $columnNames = '';
+        $columnValues = '';
+        foreach ($this->columns as $fieldName => $columnObj)
+        {
+            if ($columnObj->columnName == null) {
+                $columnNames .= $fieldName.', ';
+            } else {
+                $columnNames .= $columnObj->columnName.', ';
+            }
+            $columnValues .= $columnObj->generateValueForSQL().', ';
+        }
+        $columnNames = substr($columnNames, 0, -1);
+        $columnValues = substr($columnValues, 0, -1);
+        $thisTableName = $this->tableName;
+        $thisId = $this->id;
+
+        if ($this->id == 0) {
+            // This object is not saved yet.
+            $query = 'INSERT INTO $thisTableName ($columnNames) VALUES ($columnValues);';
+            // Execute query
+        } else {
+            // This object is on the DB.
+            $query = 'INSERT INTO $thisTableName ($columnNames) VALUES ($columnValues) WHERE id=$thisId;';
+            // Execute query
+        }
+
+        mysql_run_query($query);
+
+    }
+
+    public function update($fieldName, $_value)
+    {
+        if ($this->id == 0)
+        {
+            throw new Plasma_ObjectNotExists;
+        }
+
+        try
+        {
+            /*
+             * variable columns forms this structure -
+             * fieldName => columnObject(contains type, value, columnName)
+             */
+            if (array_key_exists($fieldName, $this->columns))
+            {
+                // make query to update
+                $this->columns[$fieldName]->setValue($_value);
+
+                $thisTableName = $this->tableName;
+                $columnName = $this->columns[$fieldName]->columnName;
+                if ($columnName == null) {
+                    $columnName = $fieldName;
+                }
+                $columnValue = $this->columns[$fieldName]->generateValueForSQL();
+                $thisId = $this->id;
+                $query = 'UPDATE $thisTableName SET $columnName=$columnValue WHERE id=$thisId';
+
+                // Execute Query
+                mysql_run_query($query);
+
+            }
+            else
+            {
+                throw new Plasma_noColumnException;
+            }
+
+        }
+        catch(Plasma_noColumnException $e)
+        {
+            // No column.
+        }
+    }
+
+    public function delete()
+    {
+        if ($this->id == 0)
+        {
+            throw new Plasma_ObjectNotExists;
+        }
+        $thisTableName = $this->tableName;
+        $thisId = $this->id;
+        $query = 'DELETE FROM $thisTableName WHERE id=$thisId';
+        // Execute Query
+        mysql_run_query($query);
+    }
+}
