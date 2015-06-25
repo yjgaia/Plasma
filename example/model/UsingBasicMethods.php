58a21ed9e5b09a410569e4612b7c736635264513
TaeMin <wwwee98@gmail.com>
2015-06-26 03:52:27 +0900

    changed usingbasicmethod example

--- example/model/UsingBasicMethods.php
+++ example/model/UsingBasicMethods.php
@@ -1,10 +1,6 @@
 <?php
-class My_Model extends Plasma_Model
+class My_ModelBox extends Plasma_ModelBox
 {
-    function function1()
-    {
-        return $this->columns['column1'];
-    }
     function __construct()
     {
 
@@ -27,25 +23,28 @@ class My_Model extends Plasma_Model
  * 우선 만든 클레스를 바탕으로 인스턴스부터 생성 해 봅시다.
  */
 
-$my_model = new My_Model();
+$my_model = new My_ModelBox();
+
+// DB에서 뭔가를 찾고싶으면 이렇게 해주면 됩니다.
+$my_model_objects = $my_model->find(array('asdf'=>'asdf'));
+
+// 세로운 객체를 만들어 내고 싶다면 이렇게 하면 됩니다.
+$myNewModelObj = $my_model->createEmptyObject();
 
 /*
  * 이름을 바꿔봅시다.
  *
  * 해당 모델의 칼럼의 값을 바꾸는것은 이렇게 해주면 됩니다.
  */
-$my_model->columns['name']->setValue('홍길동');
+$myNewModelObj->columns['name']->setValue('홍길동');
 
 // 참 쉽죠? 이렇게 칼럼의 fieldName으로 불러오고, setValue라는 메서드를 실행시켜주면 됩니다.
 
 // 이번엔 이걸 저장해보겠습니다.
-$my_model->save();
+
 
 // 쉽습니다. 그냥 ->save()하면 데이터베이스에 성공적으로 저장이 됩니다.
 
 // 이번엔 지워볼께요.
-$my_model->delete();
+$myNewModelObj->delete();
 // 이렇게하면 성공적으로 지워집니다. 굳!
-
-// 그리고 저장한 함수를 실행시켜볼깨요
-$my_model->function1();
